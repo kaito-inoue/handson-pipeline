eadd8be41a9b63cdb06421dc7dba2c8c43804c2b
Kaito Inoue <ka-inoue@iret.co.jp>
2025-06-17 14:30:31 +0900

    fix file

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement
+    print("codepipelineが実行されました")
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
